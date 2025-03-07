d50215b8db3daf48511dd6b069f64a2249aac92e
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2025-03-07 16:00:32 -0500

    Fix RequestFormParam

--- src/main/java/net/rivalgames/nanoweb/annotation/RequestFormParam.java
+++ src/main/java/net/rivalgames/nanoweb/annotation/RequestFormParam.java
@@ -1,9 +1,16 @@
 package net.rivalgames.nanoweb.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @date 3/3/2025
  * @author Moose1301
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
 public @interface RequestFormParam {
     /**
      * If the parameter type is {@link io.javalin.http.UploadedFile}
